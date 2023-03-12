159b6bf92ef40fa41a3c90488ef9b1db7429ae91
Vishnu vardhan Gandla <visu15981>
2023-03-13 01:08:23 +0530

    add code file

---
+++ independencies.py
@@ -0,0 +1,8 @@
+#@title
+
+!pip install --upgrade git+https://github.com/huggingface/diffusers.git
+!pip install --upgrade git+https://github.com/huggingface/transformers/
+!pip install accelerate==0.12.0
+!pip install scipy
+!pip install ftfy
+!pip install gradio -q
